47df5e1d2d295bc5355ed6f908e37413e05fa4b6
smitrathod <92617696+SmitRathod731@users.noreply.github.com>
2021-10-16 10:47:51 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,260 @@
+let questions=[
+    {
+        numb:1,
+        question:"Jules Rimet Trophy is related to which of the following sports?",
+        answer:"Football",
+        options:[
+            "Hockey",
+            "Cricket",
+            "Football",
+            "Volley ball"
+
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which among the following was the venue of first South Asian Games?",
+        answer: "Kathmandu",
+        options: [
+            "Kathmandu",
+            "Dhaka",
+            "Colombo",
+            "New Delhi"
+
+        ]
+    },
+    {
+        numb: 3,
+        question: "Who is the President of Hockey India?",
+        answer: "Mohammad Mushtaq Ahmed",
+        options: [
+            "Mohammad Mushtaq Ahmed",
+            "Tengku Abdullah",
+            "Tapan Kumar Das",
+            "Rajinder Singh"
+
+        ]
+    },
+    {
+        numb: 4,
+        question: "Shaphee Lanphee, a traditional textile fabric, is a GI product from which of the following states?",
+        answer: "Manipur",
+        options: [
+            "Assam",
+            "Manipur",
+            "Gujarat",
+            "Goa"
+
+        ]
+    },
+    {
+        numb: 5,
+        question: "How many teams participated in the 2018 Men’s Hockey World Cup?",
+        answer: "16",
+        options: [
+            "10",
+            "15",
+            "16",
+            "20"
+
+        ]
+    },
+    {
+     
+        numb: 6,
+        question: "Grand Central Terminal, Park Avenue, New York is the world's?",
+        answer: "largest railway station",
+        options: [
+            "largest railway station",
+            "highest railway station",
+            "longest railway station",
+            "None of the above"
+
+        ]
+
+    },
+    {
+        numb: 7,
+        question: "Entomology is the science that studies?",
+        answer: "Insects",
+        options: [
+            "Behavior of human beings",
+            "Insects",
+            "The origin and history of technical and scientific terms",
+            "The formation of rocks"
+
+        ]
+
+    },
+    {
+        numb: 8,
+        question: "Eritrea, which became the 182nd member of the UN in 1993, is in the continent of",
+        answer: "Asia",
+        options: [
+            "Asia",
+            "Africa",
+            "Europe",
+            "Australia"
+
+        ]
+
+    },
+    {
+
+        numb: 9,
+        question: "Film and TV institute of India is located at",
+        answer: "Pune (Maharashtra)",
+        options: [
+            "Rajkot (Gujarat)",
+            "Pimpri (Maharashtra)",
+            "Perambur (Tamilnadu)",
+            "Pune (Maharashtra)"
+
+        ]
+    },
+    {
+        numb: 10,
+        question: "Garampani sanctuary is located at",
+        answer: "Diphu, Assam",
+        options: [
+            "Diphu, Assam",
+            "Junagarh, Gujarat",
+            "Kohima, Nagaland",
+            "Gangtok, Sikkim"
+
+        ]
+
+    },
+    {
+        numb: 11,
+        question: "The famous rock-cut temple of Kailash is at",
+        answer: "Ellora",
+        options: [
+            "Ajanta",
+            "Badami",
+            "Mahabalipuram",
+            "Ellora"
+
+        ]
+
+    },
+    {
+        numb: 12,
+        question: "The caves and rock-cut temples at Ellora are.",
+        answer: "Hindu, Buddhist and Jain", 
+       options: [
+            "Hindu and Buddhist",
+            "Buddhist and Jain",
+            "Hindu and Jain",
+            "Hindu, Buddhist and Jain"
+
+        ]
+
+    },
+    {
+        numb: 13,
+        question: "The paintings of Ajanta depict the stories of",
+        answer: "Jataka", 
+       options: [
+            "Ramayana",
+            "Mahabharta",
+            "Jataka",
+            "Panchatantra"
+
+        ]
+
+    },
+    {
+        numb: 14,
+        question: "'OS' computer abbreviation usually means ?",
+        answer: "Operating System", 
+       options: [
+            "Operating System",
+            "Open Software",
+            "Order of Significance",
+            "Optical Sensor"
+
+        ]
+
+    },
+    {
+        numb: 15,
+        question: "The nucleus of an atom consists of",
+        answer: " protons and neutrons", 
+       options: [
+            "protons",
+            " protons and neutrons",
+            "neutrons",
+            "electrons"
+
+        ]
+
+    },
+    {
+        numb: 16,
+        question: "'.MOV' extension refers usually to what kind of file?",
+        answer: "Animation/movie file", 
+       options: [
+            " Image file",
+            "Animation/movie file",
+            "Audio file",
+            " MS Office document"
+
+        ]
+
+    },
+    {
+        numb: 17,
+        question: "Which is a type of Electrically-Erasable Programmable Read-Only Memory?",
+        answer: " Flash", 
+       options: [
+            " Flange",
+            " Flash",
+            "Fury",
+            "FRAM"
+
+        ]
+
+    },
+    {
+        numb: 18,
+        question: "The purpose of choke in tube light is ?",
+        answer: "To increase the voltage momentarily", 
+       options: [
+            " To decrease the current",
+            " To increase the current",
+            " To decrease the voltage momentarily",
+            "To increase the voltage momentarily"
+
+        ]
+
+    },
+    {
+        numb: 19,
+        question: "What frequency range is the High Frequency band?",
+        answer: "3 to 30 MHz", 
+       options: [
+            "100 kHz",
+            "200KHz",
+            "3 to 30 MHz",
+            "30 to 300 MHz"
+
+        ]
+
+    },
+    {
+        numb: 20,
+        question: "'.INI' extension refers usually to what kind of file?",
+        answer: "System file", 
+       options: [
+            "System file",
+            "Hypertext related file",
+            "Image Color Matching Profile file",
+            "Image file"
+
+        ]
+
+    },
+
+
+];
